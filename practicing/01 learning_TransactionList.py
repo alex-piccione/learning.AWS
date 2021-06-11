0f7a3889217b2d71c94b668a9bb418d6620abf93
Alex Piccione <Alex.Piccione@redington.co.uk>
2021-06-11 19:50:47 +0100

    Correct transactionList lambda function

--- practicing/01 learning_TransactionList.py
+++ practicing/01 learning_TransactionList.py
@@ -8,21 +8,47 @@ def createAsset(assetCode:str, amount:float):
 
 
 def lambda_handler(event, context):
-    #.1 Parse querystring parameters
-    #params = event["queryStringParameters"]
-    print("what does \"event\" contains?")
-    for key, value in event:
-        print(key, ":", value)
-    print("parsed params")
 
-    #.2 Create return list
+    #.1 Parse querystring parameters
+    params = extractQSParams(event) 
+
+    #.2 Create return list and response body
     list = [
         createAsset("GBP", 100.00),
         createAsset("EUR", 1234.56),
         createAsset("YEN", 12000),
     ]
 
+    body = {
+        "params": {
+            "from": params["from"],
+            "to": params["to"],
+        },
+        "assets": list
+    }
+
     return {
         'statusCode': 200,
-        'body': json.dumps(list)
+        'body': json.dumps(body)
     }
+
+
+def extractQSParams(event):
+    if not ("queryStringParameters" in event):
+        raise Exception("Querystring is missed")
+    
+    params = event["queryStringParameters"]
+
+    return {
+        "from": readQSParamOrDefault(params, "from", None),
+        "to": readQSParamOrDefault(params, "to", None)
+    }
+
+
+def readQSParam(params, paramName, mandatory = True, default = None):
+    if paramName in params: return params[paramName]
+    else: raise Exception(f"{paramName} in Querystring is missed")
+
+def readQSParamOrDefault(params, paramName, default):
+    if paramName in params: return params[paramName]
+    else: return default
