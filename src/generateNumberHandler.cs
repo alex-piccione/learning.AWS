d285a378a2864f0125cb42f21e8f2da12762f058
Alex <alessandro.piccione.75@gmail.com>
2021-06-19 21:19:49 +0100

    add LambdaSerializer

--- src/generateNumberHandler.cs
+++ src/generateNumberHandler.cs
@@ -1,10 +1,14 @@
+using System;
 using System.IO;
+using Amazon.Lambda.Core;
 // Amazon.Lambda.Serialization.SystemTextJson
 
 // ref: https://dzone.com/articles/under-the-hood-of-net-core-lambda-request
 
+// learning::Learning.Portfolio.RandomNumberFunction::Handle
+[assembly:LambdaSerializer(typeof(String))]
 namespace Learning.Portfolio
-{
+{    
     public class RandomNumberFunction {
         public string Handle(string input, Amazon.Lambda.Core.ILambdaContext context)
         {
