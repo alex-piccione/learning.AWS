482129b792ee9877b362ccae9a7cbeef3a194a07
Alex <alessandro.piccione.75@gmail.com>
2021-06-19 23:28:21 +0100

    Use Serializer properly

--- src/generateNumberHandler.cs
+++ src/generateNumberHandler.cs
@@ -1,17 +1,18 @@
 using System;
 using System.IO;
 using Amazon.Lambda.Core;
-using Learning.Portfolio;
+using Portfolio;
 // Amazon.Lambda.Serialization.SystemTextJson
 
 // ref: https://dzone.com/articles/under-the-hood-of-net-core-lambda-request
 
 // learning::Learning.Portfolio.RandomNumberFunction::Handle
 [assembly:LambdaSerializer(typeof(Request))]
-namespace Learning.Portfolio
-{    
+namespace Portfolio
+{
     public class RandomNumberFunction {
-        public string Handle(Request input, Amazon.Lambda.Core.ILambdaContext context)
+        Random fate = new Random(DateTime.Now.Millisecond);
+        public string Handle(string input, ILambdaContext context)
         {
             // Read the stream into a string
             /*if (input != null)
@@ -22,11 +23,22 @@ namespace Learning.Portfolio
             
             //var output = new StreamReader(new TextReader(""));
             //output.Write("123");
-            return "456";
+            
+            return fate.Next().ToString();
         }
-    }   
+    }
 
-    public class Request {
+    public class Request : ILambdaSerializer
+    {
+        public T Deserialize<T>(Stream requestStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Serialize<T>(T response, Stream responseStream)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
 
