2ba3c64b63671c7930a16bee65779cf831f08c18
Alex <alessandro.piccione.75@gmail.com>
2021-06-20 11:11:10 +0100

    Add test project. change deploy to build the project

---
+++ src/Tests/RandomNumber.tests.fs
@@ -0,0 +1,25 @@
+﻿module Learning.Tests
+
+open Amazon.Lambda.APIGatewayEvents
+open Amazon.Lambda.Core
+
+open NUnit.Framework;
+open FsUnit;
+
+open Learning.Portfolio
+
+
+[<Test>]
+let Get () =
+    let func = RandomNumber()
+
+    let max = 1
+
+    let context = Foq.Mock<ILambdaContext>().Create()
+    let request = APIGatewayProxyRequest()
+    request.QueryStringParameters <- dict([ ("max", max.ToString()) ])
+
+    let response = func.Handle(request, context)
+
+    response.StatusCode |> should equal 200
+    int(response.Body) |> should not' (be greaterThan max)
