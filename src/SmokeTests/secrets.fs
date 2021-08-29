45ad60157ebb0f6d85d6d857e2532e50b30cae1d
Alex <alessandro.piccione.75@gmail.com>
2021-08-29 17:30:17 +0100

    correct secrets check

--- src/SmokeTests/secrets.fs
+++ src/SmokeTests/secrets.fs
@@ -2,6 +2,7 @@
 
 open Microsoft.Extensions.Configuration
 
+
 let configuration =
     ConfigurationBuilder()
         .AddUserSecrets("learning.026d69da-e3fc-4abe-a3f4-068da978c308")
@@ -9,9 +10,9 @@ let configuration =
 
 let loadSecrets path =
     match configuration.[path] with
-    | "" -> failwith $"Secret with path {path} is empty"
+    | null -> failwith $"""Secret with path "{path}" is null."""
     | value ->  value
 
-let url =       loadSecrets "AWS:url"
+let url =       loadSecrets "AWS:url aaa"
 let accessKey = loadSecrets "AWS:access key"
 let secretKey = loadSecrets "AWS:secret key"
