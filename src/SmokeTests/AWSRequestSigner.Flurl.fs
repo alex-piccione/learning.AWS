d3ab6edfc8ba6a5efb2a33325696803cfe463571
Alex <alessandro.piccione.75@gmail.com>
2021-08-29 16:28:48 +0100

    Create AWS signer extension for Flurl

---
+++ src/SmokeTests/AWSRequestSigner.Flurl.fs
@@ -0,0 +1,91 @@
+﻿module AWSRequestSignerFlurl
+
+open System
+open System.Collections.Generic
+open System.Text
+open System.Web
+open System.Linq
+open System.Runtime.CompilerServices
+open System.Security.Cryptography
+open Flurl.Http
+
+[<Extension>]
+type Extensions() =
+    [<Extension>]
+    static member Sign(request:IFlurlRequest, method:string, content:string option, service:string, region:string) = 
+        let url = request.Url.Path
+
+        let querystring = request.Url.Query
+        let content = content.Value
+
+        if not(request.Headers.Contains("Host")) then
+            request.Headers.Add("Host", request.Url.Host)
+
+        let headers = request.Headers |> Seq.map (fun nv -> nv.ToTuple()) |> dict
+
+        let generatedHeaders = AWSRequestSigner.createAuhtorizationHeaders(method, url, querystring, content, headers, service, region)
+        request.WithHeaders(generatedHeaders)
+
+
+
+// AWS Signature Version 4
+
+// https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+
+
+let getCanonicalQueryParams(querystring:string) =
+
+    let values = SortedDictionary<string, IEnumerable<string>>(StringComparer.Ordinal)
+
+    let querystringData = HttpUtility.ParseQueryString(querystring)
+    for key in querystringData.AllKeys do
+
+        let escapedKey = Uri.EscapeDataString(key)
+        let value = querystringData.[key]
+        if value = null 
+        then
+            values.Add(escapedKey, [| $"{escapedKey}=" |])
+        else
+            // Handles multiple values per query parameter
+            let queryValues = 
+                value.Split(',')
+                    // Order by value alphanumerically (required for correct canonical string)
+                    .OrderBy(fun v -> v)
+                    // Query params must be escaped in upper case (i.e. "%2C", not "%2c").
+                    .Select(fun v -> $"{escapedKey}={Uri.EscapeDataString(v)}");
+
+            values.Add(escapedKey, queryValues)
+
+    let queryParams = values.SelectMany(fun a -> a.Value)
+    String.Join("&", queryParams)
+
+let getCanonicalHeaders headers =
+    ("", "")
+
+let calculateAuthorization method path querystring headers =
+
+    // create canonical request
+    (*
+        CanonicalRequest =
+            HTTPRequestMethod + '\n' +
+            CanonicalURI + '\n' +
+            CanonicalQueryString + '\n' +
+            CanonicalHeaders + '\n' +
+            SignedHeaders + '\n' +
+            HexEncode(Hash(RequestPayload))
+    *)
+
+    //let hexEncode input = HexEncode 
+    //String.Concat(hash.Select( fun b -> b.ToString("X2")))
+
+    let url = System.Web.HttpUtility.UrlEncodeUnicode $"{secrets.url}/{path}"
+
+    let hmac = new HMACSHA256(Encoding.UTF8.GetBytes(secrets.secretKey))
+
+    let (canonicalHeaders, headersList) = getCanonicalHeaders(headers)
+
+    let hashedPayload = hash()
+
+    let canonicalRequest = $"{method}\n{url}\n{getCanonicalQueryParams(querystring)}\n{canonicalHeaders}\n{headersList}\n{hashedPayload}"
+
+    ""
--- src/SmokeTests/AWSRequestSigner.fs
+++ src/SmokeTests/AWSRequestSigner.fs
@@ -1,6 +1,8 @@
-﻿module AWSRequestSignerV4
-
-// https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+﻿module AWSRequestSigner
+(* 
+AWS request signer V4
+https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+*)
 
 open System
 open System.Text
@@ -38,11 +40,11 @@ module private helper =
         let kService = HmacSha256(kRegion, serviceName)
         HmacSha256(kService, "aws4_request")
 
-    let GetCanonicalQueryParams(request:HttpRequestMessage) =
+    (*let GetCanonicalQueryParams(request:HttpRequestMessage) =
 
         let values = SortedDictionary<string, IEnumerable<string>>(StringComparer.Ordinal)
 
-        let querystring = HttpUtility.ParseQueryString(request.RequestUri.Query)
+        let querystring = HttpUtility.ParseQueryString()
         for key in querystring.AllKeys do
 
             let escapedKey = Uri.EscapeDataString(key)
@@ -64,7 +66,115 @@ module private helper =
         let queryParams = values.SelectMany(fun a -> a.Value)
         String.Join("&", queryParams)
 
+        *)
+
+    let GetCanonicalQueryParams(queryString:string) =
+
+        let values = SortedDictionary<string, IEnumerable<string>>(StringComparer.Ordinal)
+
+        let querystring = HttpUtility.ParseQueryString(queryString)
+        for key in querystring.AllKeys do
+
+            let escapedKey = Uri.EscapeDataString(key)
+            let value = querystring.[key]
+            if value = null 
+            then 
+                values.Add(escapedKey, [| $"{escapedKey}=" |])
+            else
+                // Handles multiple values per query parameter
+                let queryValues = 
+                    value.Split(',')
+                        // Order by value alphanumerically (required for correct canonical string)
+                        .OrderBy(fun v -> v)
+                        // Query params must be escaped in upper case (i.e. "%2C", not "%2c").
+                        .Select(fun v -> $"{escapedKey}={Uri.EscapeDataString(v)}");
+
+                values.Add(escapedKey, queryValues)
+
+        let queryParams = values.SelectMany(fun a -> a.Value)
+        String.Join("&", queryParams)
+
+
+//type AmazonCredentials = {
+//    AccessKey:string 
+//    Secret:string
+//}
+
 open helper
+let createAuhtorizationHeaders(method:string, path:string, queryString:string, content:string, headers:IDictionary<string, string>, service:string, region:string) = //timeOffset:Option<TimeSpan> 
+    
+        if String.IsNullOrEmpty(secrets.accessKey) then
+            raise (ArgumentNullException("accessKey", "Not a valid access_key."))
+
+        if String.IsNullOrEmpty(secrets.secretKey) then
+            raise(ArgumentNullException("secretKey", "Not a valid secret_key."))
+
+        if String.IsNullOrEmpty(service) then 
+            raise(ArgumentOutOfRangeException(nameof(service), service, "Not a valid service."))
+
+        if String.IsNullOrEmpty(region) then
+            raise(ArgumentOutOfRangeException(nameof(region), region, "Not a valid region."))
+
+
+        let newHeaders = Array.create<(string * string)> 3 ("", "")
+
+        let contentBytes = if content.Length > 0 then Encoding.UTF8.GetBytes(content) else Array.Empty<byte>()
+        let hashedContent = Hash(contentBytes)
+        newHeaders.[0] <- ("x-amz-content-sha256", hashedContent)
+
+        //let t = match timeOffset with
+        //        | None -> DateTimeOffset.UtcNow
+        //        | t -> DateTimeOffset.UtcNow.Add(t.Value)
+        
+        // what is the use of a time offset?
+        let time = DateTimeOffset.UtcNow
+
+        let dateAndTime = time.ToString("yyyyMMddTHHmmssZ")
+        newHeaders.[1] <- ("x-amz-date", dateAndTime)
+
+        let date = time.ToString("yyyyMMdd")
+
+        let canonicalRequest = StringBuilder()
+        canonicalRequest.Append $"{method}\n" |> ignore
+       
+        canonicalRequest.Append (String.Join("/", path.Split('/').Select(Uri.EscapeDataString)) + "\n") |> ignore
+
+        let canonicalQueryParams = GetCanonicalQueryParams(queryString)
+
+        canonicalRequest.Append (canonicalQueryParams + "\n") |> ignore
+        
+        let signedHeadersList = List<string>()
+
+        let headersToSign = (headers.Concat(newHeaders.Where( fun (h,v) -> h <> "") |> dict)).ToDictionary((fun kv -> kv.Key), fun kv -> kv.Value)
+
+        for header in headersToSign.OrderBy(fun a -> a.Key.ToLowerInvariant(), StringComparer.OrdinalIgnoreCase) do
+            canonicalRequest.Append (header.Key.ToLowerInvariant()) |> ignore
+            canonicalRequest.Append ":" |> ignore
+            // TODO manage mutivalue headers
+            //canonicalRequest.Append (String.Join(",", header.Value.Select(fun s -> s.Trim()))) |> ignore
+            canonicalRequest.Append header.Value |> ignore
+            canonicalRequest.Append "\n" |> ignore
+            signedHeadersList.Add(header.Key.ToLowerInvariant())
+
+        canonicalRequest.Append("\n") |> ignore
+
+        let signedHeaders = String.Join(";", signedHeadersList)
+
+        canonicalRequest.Append (signedHeaders + "\n") |> ignore
+        canonicalRequest.Append hashedContent |> ignore
+
+        let credentialScope = $"{date}/{region}/{service}/aws4_request"
+
+        let stringToSign = $"{ALGORITHM}\n{dateAndTime}\n{credentialScope}\n" + Hash(Encoding.UTF8.GetBytes(canonicalRequest.ToString()))
+
+        let signingKey = GetSignatureKey(secrets.secretKey, date , region, service)
+        let signature = ToHexString(HmacSha256(signingKey, stringToSign))
+
+        newHeaders.[2] <- ("Authorization", $"{ALGORITHM} Credential={secrets.accessKey}/{credentialScope}, SignedHeaders={signedHeaders}, Signature={signature}")
+        newHeaders
+
+
+
 let Sign(request:HttpRequestMessage, service:string, region:string, timeOffset:Option<TimeSpan> ) =
     
     if String.IsNullOrEmpty(secrets.accessKey) then
@@ -110,7 +220,7 @@ let Sign(request:HttpRequestMessage, service:string, region:string, timeOffset:O
        
     canonicalRequest.Append (String.Join("/", request.RequestUri.AbsolutePath.Split('/').Select(Uri.EscapeDataString)) + "\n") |> ignore
 
-    let canonicalQueryParams = GetCanonicalQueryParams(request)
+    let canonicalQueryParams = GetCanonicalQueryParams(request.RequestUri.Query)
 
     canonicalRequest.Append (canonicalQueryParams + "\n") |> ignore
 
@@ -136,7 +246,7 @@ let Sign(request:HttpRequestMessage, service:string, region:string, timeOffset:O
 
     let signingKey = GetSignatureKey(secrets.secretKey, dateStamp , region, service)
     let signature = ToHexString(HmacSha256(signingKey, stringToSign))
-        
+
     request.Headers.TryAddWithoutValidation("Authorization", $"{ALGORITHM} Credential={secrets.accessKey}/{credentialScope}, SignedHeaders={signedHeaders}, Signature={signature}") |> ignore
 
     request
