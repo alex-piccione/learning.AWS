ab66d7bb103d2983751b868d858e87e921d68331
Alex <alessandro.piccione.75@gmail.com>
2021-08-29 19:51:55 +0100

    try

--- src/SmokeTests/secrets.fs
+++ src/SmokeTests/secrets.fs
@@ -12,7 +12,7 @@ let configuration =
 let loadSecrets path env =
     match configuration.[path] with
     | null -> 
-        match Environment.GetEnvironmentVariable(env, EnvironmentVariableTarget.Machine) with 
+        match Environment.GetEnvironmentVariable(env) with 
         | null -> failwith $"""Secret with path "{path}" and Environment variable "{env}" are null."""
         | value ->  value
     | value ->  value
