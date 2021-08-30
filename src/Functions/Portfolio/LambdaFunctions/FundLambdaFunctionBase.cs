cf69e6812011d353687a73094436e259706f35b1
Alex <alessandro.piccione.75@gmail.com>
2021-08-30 12:48:12 +0100

    disable smoke tests. Use configuration file for MongoDB Donnection.

--- src/Functions/Portfolio/LambdaFunctions/FundLambdaFunctionBase.cs
+++ src/Functions/Portfolio/LambdaFunctions/FundLambdaFunctionBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 
 namespace Learning.Portfolio
 {
@@ -13,8 +14,16 @@ namespace Learning.Portfolio
 
         public FundLambdaFunctionBase()
         {
-            var connectionString = Environment.GetEnvironmentVariable("MONGODB_CONNECTION_STRING");
-            if (connectionString == null) throw new ArgumentNullException("Environment variable \"MONGODB_CONNECTION_STRING\" not found.");
+            var configFile = "configuration.json";
+            var variable = "MongoDB_connection_string";
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile(configFile)
+                .Build();
+
+            var connectionString = configuration[variable];
+            if (connectionString == null) throw new Exception($@"Cannot find ""{variable}"" in ""{configFile}"".");
+
             repository = new MongoDBFundRepository(connectionString);
         }
     }
